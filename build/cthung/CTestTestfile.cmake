0a0c88d7c6e8cdccb42526bed4752f976e08b5cf
CT-Hung <poilkj09862@gmail.com>
2019-06-27 12:53:01 +0800

    remove build and devel

--- build/cthung/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/isaiah/ros_tutorial_ws/src/cthung
-# Build directory: /home/isaiah/ros_tutorial_ws/build/cthung
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/isaiah/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/isaiah/ros_tutorial_ws/src/isaiah
-# Build directory: /home/isaiah/ros_tutorial_ws/build/isaiah
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
